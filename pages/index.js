d3d5361f02b7abdd21d673a7c5e58f69c35621c8
saidugurlu <ugurlumsaid@gmx.de>
2022-06-16 15:53:13 +0200

    main section added in index.js

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,16 @@ export default function Home() {
         dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
       />
       <main className="w-full h-full">
-        <h1 className="text-red-500 dark:text-gray-800">hello world</h1>
+        <section className="h-full max-w-5xl mx-auto pt-16 md:pt-8">
+          <div className="flex flex-col items-center justify-center md:items-start">
+            <h4 className="text-lg text-gray-800 dark:text-white">
+              <span className="text-xl font-normal">Hello,</span> I'm
+            </h4>
+            <h1 className="mt-5 text-xl font-extrabold tracking-normal text-gray-800 uppercase md:text-4xl md:mt-3 dark:text-white">
+              M. Said Ugurlu
+            </h1>
+          </div>
+        </section>
       </main>
     </Layout>
   );
