175ac7107f32dd85e00affc260a2840e3565db59
saidugurlu <ugurlumsaid@gmx.de>
2022-06-16 15:10:25 +0200

    Framer Motion animation installed and added on img

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
 import Layout from "../components/Layout";
- 
+import { motion } from "framer-motion";
+
 export default function Home() {
   return (
     <Layout title="Portfolio" description={"My Portfolio"}>
-      <img src="/images/ball-1.svg" className="hidden md:block h-auto absolute top-48 right-10 z-{1}"  alt="ball-img"/>
-      <img src="/images/ball-2.svg" className="hidden md:block h-auto absolute bottom-10 left-10 z-{1}"  alt="ball-img"/>
+      <motion.img
+        src="/images/ball-1.svg"
+        className="hidden md:block h-auto absolute top-48 right-10 z-{1}"
+        alt="ball-img"
+        whileHover={{ y: -10, rotate: 90, transition: { duration: 0.4 } }}
+        drag
+        dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+      />
+      <motion.img
+        src="/images/ball-2.svg"
+        className="hidden md:block h-auto absolute bottom-10 left-10 z-{1}"
+        alt="ball-img"
+        whileHover={{ y: -10, rotate: 90, transition: { duration: 0.4 } }}
+        drag
+        dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+      />
       <main>
         <h1 className="text-red-500">hello world</h1>
       </main>
