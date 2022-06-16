0cb09fb9c112dd28b3138abf39c654f37155c559
saidugurlu <ugurlumsaid@gmx.de>
2022-06-16 17:59:19 +0200

    react-typical work

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
-import Header from  "../components/Header";
-import React from "react";
+import Header from "../components/Header";
 
 const Layout = ({ title, description, children }) => {
   return (
     <>
-      {" "}
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
@@ -14,12 +12,11 @@ const Layout = ({ title, description, children }) => {
         <meta property="og:title" content="{title}" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="relative z-10 px-3 min-h-screen overflow-hidden bg-primary-light dark:bg-gray-900  antialiased font-montserrat"> 
-      <div className="w-full h-full ">
-<Header></Header>
-      {children}
-      </div>
-      
+      <div className="relative z-10 px-3 min-h-screen overflow-hidden bg-primary-light dark:bg-gray-900  antialiased font-montserrat">
+        <div className="w-full h-full ">
+          <Header></Header>
+          {children}
+        </div>
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
 import Layout from "../components/Layout";
 import { motion } from "framer-motion";
+import Typical from "react-typical";
+import { memo } from "react";
+ 
+
+const TypingAnimation =  memo( function  TypingAnimation () {
+    return (
+      <Typical
+        loop={3}
+        wrapper="p"
+        steps={[
+          "React Developer",
+          2000,
+          "React Enthusiast",
+          2000,
+          "Youtuber 👑",
+          2000,
+        ]}
+      />
+    );
+  },
+  (props, prevProps) => true
+);
 
 export default function Home() {
   return (
@@ -24,10 +46,15 @@ export default function Home() {
       <main className="w-full h-full">
         <section className="h-full max-w-5xl mx-auto pt-16 md:pt-8">
           <div className="flex flex-col items-center justify-center md:items-start">
-            <h4 className="text-lg text-gray-800 dark:text-white"><span className="text-xl font-normal">Hello,</span>I&rsquo;m</h4>
+            <h4 className="text-lg text-gray-800 dark:text-white">
+              <span className="text-xl font-normal">Hello,</span>I&rsquo;m
+            </h4>
             <h1 className="mt-5 text-xl font-extrabold tracking-normal text-gray-800 uppercase md:text-4xl md:mt-3 dark:text-white">
               M. Said Ugurlu
             </h1>
+            <div className="mt-5 text-2xl font-normal text-transparent bg-gradient-to-r from-pink-500 via-indigo-400 to-purple-700 bg-clip-text md:mt-3">
+              <TypingAnimation />
+            </div>
           </div>
         </section>
       </main>
