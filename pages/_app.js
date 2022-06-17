5409e7442fffdbb043a4a6a58cd9b8584f51afdc
saidugurlu <ugurlumsaid@gmx.de>
2022-06-17 18:40:09 +0200

    nextjs-progressbar added and work

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import fuchsia from "tailwindcss/colors";
+import NextNProgress from "nextjs-progressbar";
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <NextNProgress color={fuchsia[500]} height={5} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
